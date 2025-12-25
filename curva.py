b71086989ca06eec16da23f9ed605730f1f65f49
belsousa <82830112+belsousa@users.noreply.github.com>
2025-12-25 19:45:24 -0300

    fototransistor 80

--- curva.py
+++ curva.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Lê o CSV usando vírgula como separador
-df = pd.read_csv("C:\Users\ADM\Documents\lab materiais\fototransistor60.csv", sep=',')
+df = pd.read_csv("C:\Users\ADM\Documents\lab materiais\fototransistor80.csv", sep=',')
 
 # Converte tudo para string e depois para número
 df["VCE [V]"] = pd.to_numeric(df["VCE [V]"].astype(str).str.replace(',', '.'), errors='coerce')
